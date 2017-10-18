05af6431ef7aba936d32c25ee91f4aaed9f28c87
Logan Karney <karneyl@mail.gvsu.edu>
2017-10-17 23:30:52 -0400

    Update SocialFilter.java

--- src/application/SocialFilter.java
+++ src/application/SocialFilter.java
@@ -1,5 +1,10 @@
 package application;
 
+/*******************************************************************************
+ * Class that determines which Social Media API is being used
+ *
+ * @author Logan Karney
+ ******************************************************************************/
 public enum SocialFilter {
 	TWITTER, INSTAGRAM, FACEBOOK;
 }
