80e47595f6c8b2095800c4250b0bd72332c92b05
sciarian <sciarian@mail.gvsu.edu>
2017-10-18 13:54:14 -0400

    Update TwitterTest.java

--- src/apis/TwitterTest.java
+++ src/apis/TwitterTest.java
@@ -1,105 +1,120 @@
-package faceBookAndTwitterOLD;
+package application;
 
-import static org.junit.Assert.*;
+import javafx.geometry.Insets;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import twitter4j.User;
 
-import java.util.ArrayList;
-
-import org.junit.Test;
-
-import apis.Tweet;
-import apis.TwitterSearch;
-import apis.TwitterSnooper;
-import application.TwitterProfile;
-import twitter4j.TwitterException;
-
-/*****************************************************************
- * This program will test all the classes used to pull data from 
- * twitter users with the twitter4j API.
- * @author Anthony Sciarini
+/*******************************************************************************
+ * Creates a user profile generated from data taken from the Twitter api
  *
- ******************************************************************/
-public class TwitterTest {
+ * @author Logan Karney
+ ******************************************************************************/
+public class TwitterProfile {
 
-	//Set up testing
-	private TwitterSnooper test = new TwitterSnooper("jYIuPAqqJtnE8GoIA1NmBXjs3","E3Jym5p4p6rbZhFxNc7FWbJut06eASAF2h5aY2sVoea1Zitear");
-	private TwitterSearch search;
-	private TwitterProfile profile;
-	private Tweet tweet;
-	
-	public TwitterTest(){
-		try {
-			 search = test.searchUser("sciarian");
-			 profile = search.getProfiles().get(0);
-			 tweet = search.tweets.get(0);
-		} catch (TwitterException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	private BorderPane pane;
+	/** The first and last name of the twitter user. */
+	protected Text name;
+	// private String name, screenName, bio, tweetDate;
+
+	/** The screen name of the twitter profile. */
+	protected Text screenName;
+
+	/** The users bio. */
+	private Text bio;
+
+	/** The users profile picture. */
+	protected ImageView profilePic;
+	protected Image image;
+
+	/** The URL of the users home twitter page. */
+	private String url;
+
+	// private ImageIcon profilePic;
+
+	/**********************************************
+	 * Takes a object of User, u, and pulls profile information from the user
+	 * object.
+	 * 
+	 * @param u
+	 *            A user.
+	 *********************************************/
+	public TwitterProfile(User u) {
+		// name = new Text(u.getName()); // Gets the name of the user.
+		name = new Text(u.getName());
+		// screenName = new Text(u.getScreenName()); // Gets the screen name of
+		// the
+		// user.
+		screenName = new Text(u.getScreenName());
+		// bio = new Text(u.getDescription()); // Gets the user description.
+		setBio(new Text(u.getDescription()));
+		url = u.getURL(); // Gets the URL of the user's twitter page.
+		image = new Image(u.getBiggerProfileImageURL());
+
+		profilePic = new ImageView(image);
+		// https://stackoverflow.com/questions/27894945/how-do-i-resize-an-imageview-image-in-javafx,
+		// by James_D
+		profilePic.setFitHeight(100);
+		profilePic.setFitWidth(100);
+
+		profilePic.setPreserveRatio(true);
+		BorderPane.setMargin(profilePic, new Insets(12, 12, 8, 12));
+
+		name.setId("name-text");
+
+		VBox textHolder = new VBox();
+		textHolder.getChildren().addAll(name, getBio());
+		VBox.setMargin(name, new Insets(10, 10, 0, 0));
+		VBox.setMargin(getBio(), new Insets(0, 10, 10, 10));
+
+		pane = new BorderPane();
+		pane.setLeft(profilePic);
+		pane.setCenter(textHolder);
+
+		pane.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+	}
+
+	/******************************************************************************
+	 * @return pane
+	 *****************************************************************************/
+	public BorderPane getPane() {
+		return pane;
+	}
+
+	/********************************************************
+	 * Gets the twitter user bio.
+	 * @return	returns the a text object of the user bio.
+	 ********************************************************/
+	public Text getBio() {
+		return bio;
 	}
 	
-	//Test TwitterSearch.
-	
-	@Test
-	public void testSearchPopulatesProfileList() {
-		assertTrue(search.getProfiles().size() > 0);	
+	/***************************************
+	 * Sets the bio of the user profile.
+	 * @param bio a object of text
+	 **************************************/
+	public void setBio(Text bio){
+		this.bio = bio;
+	}
+
+	/*********************************************
+	 * Gets the twitter user name.
+	 * @return returns a Text of the user name.
+	 *********************************************/
+	public Text getName() {
+		// TODO Auto-generated method stub
+		return name;
 	}
 	
-	@Test
-	public void testSearchPopulatesTweetList() {
-		assertTrue(search.tweets.size() > 0);	
+	/*******************************************************************
+	 * Gets the screen name of the twitter user.
+	 * @return returns a Text object of the twitter users screen name.
+	 ******************************************************************/
+	public Text getScreenName(){
+		return screenName;
 	}
-	
-	//Test TwitterProfile.
-	
-	@Test
-	public void testGetUserBio(){
-		assertEquals(profile.getBio(), "My Bio");
-	}
-	
-	@Test
-	public void testGetUserName(){
-		assertEquals(profile.getName(), "Anthony Sciarini");
-	}
-	
-	@Test
-	public void testGetUserScreenName(){
-		assertEquals(profile.getScreenName(), "sciarian");
-	}
-	
-	@Test 
-	public void getUserTimeLine(){
-		ArrayList<Tweet> tweets;
-		try {
-			tweets = test.getUserTimeLine();
-			assertTrue(tweets.size() > 0);
-		} catch (TwitterException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	//Test tweet
-	
-	@Test
-	public void getTweetTime(){
-		assertEquals(tweet.getTweetTime(), "Oct 8");
-	}
-	
-	@Test
-	public void getTweetUserName(){
-		assertEquals(tweet.getName(), "Anthony Sciarini");
-	}
-	
-	@Test
-	public void getTweetUserScreenName(){
-		assertEquals(tweet.getScreenName(), "sciarian");
-	}
-	
-	@Test
-	public void getUserTweet(){
-		assertEquals(tweet.getTweet(), "Hello World");
-	}
-	
-	
-	
 }
