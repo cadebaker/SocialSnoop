40990dbd75584db38202e90dd1f7b3ef46006780
Luke Bassett <31935820+BrotherB1@users.noreply.github.com>
2017-12-05 00:11:14 -0500

    Added pulling posts with some info

--- InstagramProfile.java
+++ InstagramProfile.java
@@ -10,16 +10,18 @@ import java.net.URLConnection;
 import java.nio.charset.Charset;
 import java.util.HashMap;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class InstagramProfile {
-public static void main(String[] args) throws IOException, JSONException {	
+	
+public static void main(final String[] args) throws IOException, JSONException {	
 		
 		/** String value that holds the access token. **/
 		String Access ="6188961345.8504729.30f2ce5d68654477a79cf32dbd94525b";
 		
-		/** Url that is the same (besides the access token) to get user information **/
+		/** Url that is the same anytime (besides the access token) to get user information **/
 		URL url = new URL("https://api.instagram.com/v1/users/self/?access_token=" + Access);
 		
 		/**Connection to get access to the Web**/
@@ -50,7 +52,7 @@ public static void main(String[] args) throws IOException, JSONException {
 	 * 
 	 * @param rd is a BufferedReader that reads the JSON web file
 	 *************************************************************************/
-	private static String readAll(Reader rd) throws IOException {
+	private static String readAll(final Reader rd) throws IOException {
 		    StringBuilder sb = new StringBuilder();
 		    int cp;
 		    while ((cp = rd.read()) != -1) {
@@ -66,7 +68,7 @@ public static void main(String[] args) throws IOException, JSONException {
 	 * @param j is the JSON object that holds the 'data' JSONArray
 	 * 				within the original JSON object.
 	 **************************************************************************/
-	private static HashMap<String,String> user(JSONObject j) throws JSONException {
+	private static HashMap<String,String> user(final JSONObject j) throws JSONException {
 		
 		/**JSONObject.get() returns an object, so object x stores
 		 * that value until we can call toString on it.
@@ -91,5 +93,44 @@ public static void main(String[] args) throws IOException, JSONException {
 		info.put("username", x.toString());
 		
 		return info;
+	}
+	
+public static HashMap<String,String> getPosts(String Access) throws IOException, JSONException{
+		
+		/** String value that holds the access token. **/
+		String Acces ="241274222.8504729.5e561ee33eca44b1a675b41af138c063";
+		
+		/** Url that is the same (besides the access token) to get user information **/
+		URL url = new URL("https://api.instagram.com/v1/users/self/media/recent?access_token=" + Access);
+		
+		/**Connection to get access to the Web**/
+		URLConnection con = url.openConnection();
+	    InputStream is =con.getInputStream();
+	    
+	     
+	    /**BufferedReader to read the online files and put it into a JSON **/
+	    BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+	    String jsonText = readAll(rd);
+	    JSONObject json = new JSONObject(jsonText);
+//	    System.out.println(json);
+//	    System.out.println(json.get("data"));
+//	    System.out.println(json.optJSONObject("data"));
+	    JSONArray j = json.optJSONArray("data");
+	    
+	    System.out.println(j);
+	    JSONObject post0 = j.getJSONObject(0);
+	    System.out.println(post0);
+	    
+	    
+		HashMap<String,String> posts = new HashMap<String, String>();//getPostInfo(json);
+		System.out.println(post0.get("images"));
+		posts.put("picture", post0.optJSONObject("images").optJSONObject("standard_resolution").get("url").toString());
+		posts.put("caption", post0.optJSONObject("caption").get("text").toString());
+		posts.put("likes", post0.optJSONObject("likes").get("count").toString());
+		JSONArray people = post0.optJSONArray("users_in_photo");
+		posts.put("tagged person 1", people.getJSONObject(0).optJSONObject("user").get("username").toString());
+		posts.put("tagged person 2", people.getJSONObject(1).optJSONObject("user").get("username").toString());
+		
+		return posts;
 	}
 }
